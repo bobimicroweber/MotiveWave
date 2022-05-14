9cd289f068458e9f32749162d07d87fb722804d3
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2022-05-14 11:03:28 +0300

    Create BigVolumeStrategy.java

---
+++ BigVolumeStrategy.java
@@ -0,0 +1,42 @@
+package study_examples;
+
+import com.motivewave.platform.sdk.order_mgmt.OrderContext;
+import com.motivewave.platform.sdk.study.Study;
+import com.motivewave.platform.sdk.study.StudyHeader;
+
+@StudyHeader(
+  namespace="com.cloudvisionltd", 
+  id="BIG_VOLUME_STRATEGY", 
+  name="Big Volume Strategy",
+  desc="Catch big volume",
+  menu="A4Crypto.com",
+  overlay = true,
+  signals = true,
+  strategy = true,
+  autoEntry = true,
+  manualEntry = false,
+  supportsUnrealizedPL = true,
+  supportsRealizedPL = true,
+  supportsTotalPL = true)
+
+public class BigVolumeStrategy extends Study
+{
+  @Override
+  public void onActivate(OrderContext ctx)
+  {
+    if (getSettings().isEnterOnActivate()) {
+      
+     
+      
+    }
+  }
+
+  @Override
+  public void onSignal(OrderContext ctx, Object signal)
+  {
+   
+	  
+  
+    
+  }
+}
